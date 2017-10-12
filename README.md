2ed38d698a2a61f047ae9b863cc854eb5762cd09
hzfangwentian <hzfangwentian@corp.netease.com>
2017-10-12 22:40:36 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # osmanthus
+> Mock server for BubbleTeam
+
+Osmanthus桂花，开发的时候，园区里的桂花开了，很香，因而得名。
+
+```
+Usage: oss -e feature1 | oss -u http://10.240.177.151:8020 | oss -l | oss
+
+选项：
+  --version    显示版本号                                                 [布尔]
+  --env, -e    specify a test environment to connect
+  --url, -u    specify an url to connect
+  -h           显示帮助信息                                               [布尔]
+  --local, -l  local mode                                                 [布尔]
+```
--- main/index.js
+++ main/index.js
@@ -2,7 +2,7 @@ const spawn = require('cross-spawn');
 const path = require('path');
 const cwd = process.cwd();
 const processEnvs = require('../lib/processEnvs');
-const mcss = require('../lib/mcss');
+const bootMcss = require('../lib/mcss');
 const mockServer = require('../lib/mockServer');
 const config = require(path.join(cwd, 'osmanthus.js'));
 const { environments, mockPort, appPath, mockServerPath } = config;
@@ -11,25 +11,39 @@ const bootServer = (url) => {
     spawn('node', ['app.js','--NODE_CONFIG={"remoteServer":"' + url + '"}'], {stdio:[0, 1, 2]});
 }
 
+/*
+    与pop之前的差异：
+    - 之前：非本地mock的时候，不启动mockserver，给app.js指定remoteServer
+    - 现在：总是启动mockserver，app.js的remoteServer固定指定http://127.0.0.1:8020，仅当oss -l的时候仅启动app.js，给后端调试用。
+ */
+
 module.exports = (env, url, local) => {
     if (!config) {
         console.log('missing osmanthus.js');
         return;
     }
 
-    // mcss
-    mcss();
+    let localUrl = `http://127.0.0.1:${mockPort}`
 
-    // 连接测试环境或者指定url
-    if(env || url) {
-        let targetUrl = environments[env] || url;
-        processEnvs(env ? 'env' : 'url');
-        bootServer(targetUrl);
+    // bootMcss
+    bootMcss();
+
+    // 仅启动app.js
+    if(local) {
+        bootServer(localUrl);
         return;
     }
 
-    // 默认本地mock模式
-    processEnvs('local');
-    bootServer(`http://127.0.0.1:${mockPort}`);
-    mockServer();
+    // 连接测试环境或者指定url, 代理到url
+    let targetUrl
+    if(env || url) {
+        targetUrl = environments[env] || url;
+        processEnvs(env ? 'env' : 'url');
+    } else {
+        // 默认本地mock模式
+        processEnvs('local');
+    }
+
+    bootServer(localUrl);
+    mockServer(targetUrl);
 }
